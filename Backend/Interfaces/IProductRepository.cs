3971f754bd7ce784bf2f0b3df10faa33f2fad319
Ronen Gelmanovich <ronenge@mta.ac.il>
2024-10-02 19:24:38 +0300

    - Added more functionality to product repository

---
+++ Backend/Interfaces/IProductRepository.cs
@@ -0,0 +1,17 @@
+﻿using Backend.Models;
+using Backend.Models.Filter;
+
+namespace Backend.Interfaces;
+
+public interface IProductRepository
+{
+    ICollection<Product> GetProducts();
+    Product GetProductById(int id);
+    ICollection<Product> GetFilteredProducts(ProductFilter filter);
+    
+    void AddProduct(Product product);
+    void UpdateProduct(Product product);
+    void DeleteProduct(int id);
+    
+    bool SaveChanges();
+}
---
+++ Backend/Repositories/ProductRepository.cs
@@ -0,0 +1,77 @@
+﻿using Backend.Data;
+using Backend.Interfaces;
+using Backend.Models;
+using Backend.Models.Filter;
+
+namespace Backend.Repositories;
+
+public class ProductRepository(DataContext i_Context) : IProductRepository
+{
+    private readonly DataContext _context = i_Context;
+
+    public ICollection<Product> GetProducts()
+    {
+        return _context.Products.ToList();
+    }
+
+    public Product GetProductById(int id)
+    {
+        return _context.Products.Find(id);
+    }
+
+    public ICollection<Product> GetFilteredProducts(ProductFilter filter)
+    {
+        IQueryable<Product> query = _context.Products.AsQueryable();
+
+        if(filter.MinStock.HasValue)
+        {
+            query = query.Where(p => p.Stock >= filter.MinStock.Value);
+        }
+
+        if(filter.MaxStock.HasValue)
+        {
+            query = query.Where(p => p.Stock <= filter.MaxStock.Value);
+        }
+        
+        if(filter.MinPrice.HasValue)
+        {
+            query = query.Where(p => p.Price >= filter.MinPrice.Value);
+        }
+
+        if(filter.MaxPrice.HasValue)
+        {
+            query = query.Where(p => p.Price <= filter.MaxPrice.Value);
+        }
+
+        if (!string.IsNullOrEmpty(filter.Category))
+        {
+            query = query.Where(p => p.Category.Name == filter.Category);
+        }
+        
+        return query.ToList();
+    }
+
+    public void AddProduct(Product product)
+    {
+        _context.Products.Add(product);
+    }
+
+    public void UpdateProduct(Product product)
+    {
+        _context.Products.Update(product);
+    }
+
+    public void DeleteProduct(int id)
+    {
+        Product productToDelete = _context.Products.Find(id);
+        if(productToDelete != null)
+        {
+            _context.Products.Remove(productToDelete);
+        }
+    }
+
+    public bool SaveChanges()
+    {
+        return _context.SaveChanges() >= 0;
+    }
+}
