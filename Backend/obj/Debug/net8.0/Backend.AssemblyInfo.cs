8dde08e167cb920cac664c521dbd4efecd82fb3a
Ronen Gelmanovich <ronenge@mta.ac.il>
2024-10-02 18:04:09 +0300

    Added connection string and mysql package

--- Backend/obj/Debug/net8.0/Backend.AssemblyInfo.cs
+++ Backend/obj/Debug/net8.0/Backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5afc10e645594a3e15723a2c7f35869f011f4c9c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c8b6e4c9e7e12c6640c02804b8464c8204d56a19")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
