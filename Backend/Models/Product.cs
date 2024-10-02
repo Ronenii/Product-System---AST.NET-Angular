b82b7756c3e5a91cbf36fce5328075075103b4eb
Ronen Gelmanovich <ronenge@mta.ac.il>
2024-10-02 17:13:48 +0300

    Added product model

--- Backend/Models/Product.cs
+++ Backend/Models/Product.cs
@@ -3,5 +3,9 @@
 public class Product
 {
     public int Id { get; set; }
-    
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public float Price { get; set; }
+    public int Stock { get; set; }
+    public int Category { get; set; }
 }
