10725de100fb6bbbc5d2d48aad3e4c0dd00703f9
Ronen Gelmanovich <ronenge@mta.ac.il>
2024-10-05 12:22:51 +0300

    Created sidebar component and added global font

--- Frontend/src/app/app.routes.ts
+++ Frontend/src/app/app.routes.ts
@@ -1,9 +1,9 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-  { path: 'home', HomeComponent },
-  { path: 'login', component: LoginComponent },
-  { path: 'products', component: ProductsComponent },
-  { path: 'online-users', component: OnlineUsersComponent },
-  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  //   { path: 'home', HomeComponent },
+  //   { path: 'login', component: LoginComponent },
+  //   { path: 'products', component: ProductsComponent },
+  //   { path: 'online-users', component: OnlineUsersComponent },
+  //   { path: '', redirectTo: '/home', pathMatch: 'full' },
 ];
